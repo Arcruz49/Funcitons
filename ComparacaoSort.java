ecf23aa0fa2139394136bdbc6de235bbc66238bf
Arthur Cruz <arthur.cruz@objetivasolucao.com.br>
2023-09-14 00:42:57 -0300

    função de calcular tempo de vetores grandes implementada

--- ComparacaoSort.java
+++ ComparacaoSort.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class ComparacaoSort {
 
     public static void selecao(int[] v) {
@@ -64,6 +66,15 @@ public class ComparacaoSort {
         System.out.print("Ordenado: ");
         ImprimeVetor(vet);
     }
+
+    public static int [] GeraVetRnd(){
+        Random random = new Random();
+        int [] vet = new int[100000];
+        for(int i = 0 ; i < vet.length; i ++){
+            vet[i] = random.nextInt(0,vet.length);
+        }
+        return vet;
+    }
     public static void main(String[] args) throws Exception {
 
         int[] vetMelhorCaso1 = {1,2,3,4,5};
@@ -83,6 +94,24 @@ public class ComparacaoSort {
         ImprimeTesteCaso2(vetMelhorCaso2);
         System.out.println("-------------Pior caso-------------");
         ImprimeTesteCaso2(vetPiorCaso2);
+        
+        int[] rndVet1 = GeraVetRnd();
+        int[] rndVet2 = new int[rndVet1.length];
+        for(int i = 0; i < rndVet1.length;i++){
+            rndVet2[i] = rndVet1[i];
+        }
+
+        System.out.println("\n\n-------------Vetor aleatório-----------");
+        System.out.println("\nSELECTION DO PROFESSOR: ");
+        double tempoInicial = System.currentTimeMillis();
+        selecao(rndVet1);
+        System.out.println("O método foi executado em " + (System.currentTimeMillis() - tempoInicial)/1000);
+
+        System.out.println("\n\nSELECTION NORMAL: ");
+
+        tempoInicial = System.currentTimeMillis();
+        selectionSort(rndVet2);
+        System.out.println("O método foi executado em " + (System.currentTimeMillis() - tempoInicial)/1000);
 
     }
 }
